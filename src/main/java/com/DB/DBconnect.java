0c8a0788b1b7673f520b98a6694fee4385f5ba13
Atharva Jagtap <jagtapatharva408@gmail.com>
2024-01-29 12:29:50 +0530

    commiting for pipeline

--- src/main/java/com/DB/DBconnect.java
+++ src/main/java/com/DB/DBconnect.java
@@ -14,25 +14,26 @@ public class DBconnect {
 	private static String RDSusername = "admin";
 	private static String RDSpassword = "rootpass1";
 
-	private static String EC2username = "admin";
-	private static String EC2password = "Admin#123";
+	private static String EC2username = "atharva07";
+	private static String EC2password = "Atharva@07";
 
 	public static Connection getConn() throws SQLException {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			 //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal",
-			 //Localusername, Localpassword);
-			// conn =
-			// DriverManager.getConnection("jdbc:mysql://database-1.cw6ytvpftuvq.ap-south-1.rds.amazonaws.com:3306/jobportal",
-			// RDSusername, RDSpassword);
+//			
+//			 conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal",
+//			 Localusername, Localpassword);
+			 
+//			 conn =
+//			 DriverManager.getConnection("jdbc:mysql://database-1.cw6ytvpftuvq.ap-south-1.rds.amazonaws.com:3306/jobportal",
+//			 RDSusername, RDSpassword);
 
-			// conn =
-			// DriverManager.getConnection("jdbc:mysql://database-1.cw6ytvpftuvq.ap-south-1.rds.amazonaws.com:3306/jobportal?"+"user=admin&password=rootpass1");
-
-			conn = DriverManager
-					.getConnection("jdbc:mysql://35.154.119.128:3306/jobportal",
-							EC2username, EC2password);
+			
+			  conn = DriverManager
+			  .getConnection("jdbc:mysql://65.2.47.33:3306/jobportal", EC2username,
+			  EC2password);
+			 
 
 		} catch (Exception e) {
 			e.printStackTrace();
